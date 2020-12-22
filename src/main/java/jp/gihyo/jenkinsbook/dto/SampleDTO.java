d0a9e77943b133c3d785ffb077104b3bf7703553
root <root@localhost.localdomain>
2020-12-22 12:20:22 +0900

    test

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * DTO class for SampleServlet.
  */
-public class SampleDTO implements Serializable {
+public class SampleDTO {
     /**
      * First name of the user.
      */
