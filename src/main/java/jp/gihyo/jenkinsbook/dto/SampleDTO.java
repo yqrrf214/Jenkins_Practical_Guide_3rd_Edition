e0bef6309f02baf59ac47dfaa61db4b155ace1c7
root <root@localhost.localdomain>
2020-12-22 10:24:18 +0900

    test

--- src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
+++ src/main/java/jp/gihyo/jenkinsbook/dto/SampleDTO.java
@@ -24,9 +24,9 @@ public class SampleDTO implements Serializable {
      * @param firstName first name of the user
      * @param lastName last name of the user
      */
-    public SampleDTO(final String firstName, final String lastName) {
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
+    public SampleDTO(final String fstName, final String lstName) {
+        this.setFirstName(fstName);
+        this.setLastName(lstName);
         this.setMessage("Hello");
     }
 
